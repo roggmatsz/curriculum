723cb87b8b56f0211ae200a35e5d9669f4ab9cb4
Rogger Matamoros <roggmatz@gmail.com>
2022-03-28 21:30:45 +0000

    End of section 36. Introduced data template binding.

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+	const date = new Date();
+	const someTitle = 'tiiiiittle here';
+
 	return (
 		<div className='expense-item'>
-			<span>Date Here</span>
+			<span>{ date.toISOString() }</span>
 			<div className='expense-item__description'>
-				<h2>Title</h2>
-				<span className='expense-item__price'>Amount Here</span>
+				<h2>{ someTitle }</h2>
+				<span className='expense-item__price'>{ Math.random() }</span>
 			</div>
 		</div>
 	);
