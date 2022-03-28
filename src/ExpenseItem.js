41aaaff1a6b7450ec9b41a28440d278dfd5d98bc
Rogger Matamoros <roggmatz@gmail.com>
2022-03-28 21:22:19 +0000

    End of section 35.

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,11 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-	return (<>
-		<span>Date Here</span>
-		<div>
-			<h2>Title</h2>
-			<span>Amount Here</span>
+	return (
+		<div className='expense-item'>
+			<span>Date Here</span>
+			<div className='expense-item__description'>
+				<h2>Title</h2>
+				<span className='expense-item__price'>Amount Here</span>
+			</div>
 		</div>
-	</>);
+	);
 }
 
 export default ExpenseItem;
