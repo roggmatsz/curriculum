2f51fdac90ad80937d18b5855421ce82a8b8eca4
Rogger Matamoros <roggmatz@gmail.com>
2022-03-29 16:09:29 +0000

    Assignment 1: Create a custom component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './expenses/ExpenseItem';
+import Expenses from './expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -26,10 +26,8 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <h2>List of Expenses</h2>
+      <Expenses list={expenses} />
     </div>
   );
 }
