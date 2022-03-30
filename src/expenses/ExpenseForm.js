95e22ea6461e02ae6cb6e4d416294796d7198943
Rogger Matamoros <roggmatz@gmail.com>
2022-03-30 22:11:33 +0000

    End of section 55. Created multiple state variables and handlers for each expense form field.

--- src/expenses/ExpenseForm.js
+++ src/expenses/ExpenseForm.js
@@ -1,9 +1,22 @@
 import './ExpenseForm.css';
+import { useState } from 'react';
 
 function ExpenseForm(props) {
+	const [ enteredTitle, setEnteredTitle ] = useState('');
+	const [ enteredAmount, setEnteredAmount ] = useState('');
+	const [ enteredDate, setEnteredDate ] = useState('');
+
 	const onTitleChange = (event) => {
-		console.log(event?.target?.value);
+		setEnteredTitle(event?.target?.value);
 	};
+
+	const onAmountChange = (event) => {
+		setEnteredAmount(event?.target?.value);
+	}
+
+	const onDatechange = (event) => {
+		setEnteredDate(event?.target?.value);
+	}
 
 	return (
 		<form>
@@ -14,11 +27,11 @@ function ExpenseForm(props) {
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.1" />
+					<input onChange={onAmountChange} type="number" min="0.01" step="0.1" />
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" />
+					<input onChange={onDatechange} type="date" min="2019-01-01" max="2022-12-31" />
 				</div>
 			</div>
 			<div className="new-expense__actions">
