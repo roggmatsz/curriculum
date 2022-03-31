fd716e2beda825fad105babe30b19ca5d608af4f
Rogger Matamoros <roggmatz@gmail.com>
2022-03-31 13:58:48 +0000

    End of part 64. Introduced expressions to render multiple components.

--- src/expenses/Expenses.js
+++ src/expenses/Expenses.js
@@ -15,11 +15,9 @@ function Expenses(props) {
 	
 	return (
 		<Card className='expenses'>
-			<ExpensesFilter onSelectedFilter={onSelectedFilterHander} selected={selectedFilter} />
-			<ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
-			<ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/>
-			<ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-			<ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date}/>
+			{props.list.map(item =>
+				<ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+			)}
 		</Card>
 	);
 }
