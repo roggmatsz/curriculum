691fd38d05c4071da03923279e5f8900760bf9e9
Rogger Matamoros <roggmatz@gmail.com>
2022-03-31 17:29:00 +0000

    End of part 70. Setting up char bar functionality and introducing dynamic styling of elements.

--- src/expenses/chart/ChartBar.js
+++ src/expenses/chart/ChartBar.js
@@ -1,5 +1,18 @@
 import './ChartBar.css';
 
 export default function ChartBar(props) {
+    let barHeight = '0%';
 
+    if (props?.max > 0) {
+        barHeight = Math.round((props?.value / props?.maxValue) * 100) + '%';
+    }
+
+    return (
+        <div className='chart-bar'>
+            <div className='chart-bar__inner'>
+                <div className='chart-bar-fill' style={{'height': barHeight}}></div>
+            </div>
+            <label className='chart-bar__label'>{props.label}</label>
+        </div>
+    );
 }
