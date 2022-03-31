f7bc70a0c7c9e87cc30690fae3bc96a2b61c2e9b
Rogger Matamoros <roggmatz@gmail.com>
2022-03-31 16:13:06 +0000

    End of part 67. Another way of conditionally rendering.

--- src/expenses/Expenses.js
+++ src/expenses/Expenses.js
@@ -9,6 +9,12 @@ function Expenses(props) {
 	const filteredExpenses = props.list.filter(expense => 
 		expense?.date?.getFullYear().toString() === selectedFilter);
 
+	let expensesContent = <p>No expenses found.</p>;
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map(expense => 
+			<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+		);
+	}
 	const onSelectedFilterHander = (value) => {
 		setSelectedFilter(value);
 	}; 
@@ -16,12 +22,7 @@ function Expenses(props) {
 	return (
 		<Card className='expenses'>
 			<ExpensesFilter selected={selectedFilter} onSelectedFilter={onSelectedFilterHander} />
-			
-			{filteredExpenses.length === 0 && <p>No expenses found.</p>}
-			
-			{filteredExpenses.length > 0 && filteredExpenses.map(item => 
-				<ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
-			)}
+			{expensesContent}
 		</Card>
 	);
 }
