63230049cd69f7793bfc585db4c2e0f11c8e290e
Rogger Matamoros <rogger.matamoros@softvision.com>
2022-04-01 10:46:35 -0400

    End of part 81. Using media queries with styled components.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,43 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
 import Button from '../../UI/Button/Button';
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.valid ? 'black' : 'red'}
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? '#ccc' : 'red')};
-    background: ${props => props.invalid ? 'transparent' : '#FFD7D7'}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: #FFD7D7;
-  }
-
-  &.invalid label {
-    border-color: red;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -64,10 +27,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl valid={isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
