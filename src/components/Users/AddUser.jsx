3285c91d3e9f218d820d837f86ad8debc18b16e6
Rogger Matamoros <rogger.matamoros@softvision.com>
2022-04-01 13:36:05 -0400

    End of part 91. Practice using CSS modules and setting up custom components to accept additional classes in addition to those defined in the render function.

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,3 +1,6 @@
+import Card from '../ui/Card';
+import styles from './AddUser.module.css';
+
 export default function AddUser(props) {
 
    const onFormSubmit = (event) => {
@@ -5,12 +8,14 @@ export default function AddUser(props) {
    };
 
    return (
-      <form onSubmit={onFormSubmit}>
-         <label htmlFor="username">Username</label>
-         <input type="text" id="username" />
-         <label htmlFor="age">Age (years)</label>
-         <input type="number" id="age" />
-         <button type="submit">Submit</button>
-      </form>
+      <Card className={styles.input}>
+         <form onSubmit={onFormSubmit}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+            <label htmlFor="age">Age (years)</label>
+            <input type="number" id="age" />
+            <button type="submit">Submit</button>
+         </form>
+      </Card>
    );
 }
