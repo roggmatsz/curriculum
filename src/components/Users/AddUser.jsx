983b1da0d7d5f7fc506bb1e1b076b6598e2c09d1
Rogger Matamoros <rogger.matamoros@softvision.com>
2022-04-01 13:47:39 -0400

    End of part 93. Practice adding reactivity to a form, event handlers, creating state variables.

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import Card from '../ui/Card';
 import Button from '../ui/Button';
 import styles from './AddUser.module.css';
 
 export default function AddUser(props) {
+   const [ enteredUsername, setEnteredUsername ] = useState('');
+   const [ enteredAge, setEnteredAge ] = useState('');
+
    const onFormSubmit = (event) => {
       event.preventDefault();
+      console.log(enteredAge, enteredUsername);
    };
+
+   const onUsernameChange = (event) => {
+      setEnteredUsername(event?.target?.value);
+   }
+
+   const onAgeChange = (event) => {
+      setEnteredAge(event?.target?.value);
+   }
 
    return (
       <Card className={styles.input}>
          <form onSubmit={onFormSubmit}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input type="text" id="username" onChange={onUsernameChange} />
             <label htmlFor="age">Age (years)</label>
-            <input type="number" id="age" />
+            <input type="number" id="age" onChange={onAgeChange} />
             <Button type="submit">Submit</Button>
          </form>
       </Card>
